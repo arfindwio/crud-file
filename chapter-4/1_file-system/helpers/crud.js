965b240e8aeaba0e2cb3e7d9acc3b21bd529fdbc
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-09-25 22:25:55 +0700

    feat: add function index & show

--- chapter-4/1_file-system/helpers/crud.js
+++ chapter-4/1_file-system/helpers/crud.js
@@ -9,8 +9,28 @@ function create(title, body) {
   fs.writeFileSync("./database/posts.json", JSON.stringify(posts, null, 4));
 }
 
-function index() {}
-function show(id) {}
+function index() {
+  // Menampilkan semua data
+  // let dataDb = fs.readFileSync('./database/posts.json','utf-8');
+  let { data } = posts;
+  for (const value of data) {
+    console.log(`Data ke ${value.id} ,Judul =  ${value.title} , Body =  ${value.body}`);
+    console.log("\n");
+  }
+}
+
+function show(id) {
+  // Menampilkan Bedasarkan Id
+  if (!id) {
+    return "ID Harus Ada";
+  }
+  const showData = posts.data.find((post) => post.id === id);
+  if (!showData) {
+    return "Data Tidak Ditemukan";
+  }
+  return showData;
+}
+
 function update(id, title, body) {
   const postUpdate = posts.data.find((post) => post.id === id);
 
--- chapter-4/1_file-system/index.js
+++ chapter-4/1_file-system/index.js
@@ -1,13 +1,16 @@
 const { create, index, show, update, destroy } = require("./helpers/crud");
 
 // test create post
-// create('test title', 'test data');
+create("test title", "test data");
 
 // test show all post
+index();
+
 // test show detail post by id
+console.log(show(3));
 
 // test update post by id
-// update(1, "test123 title", "test123 data");
+update(1, "test123 title", "test123 data");
 
 // test delete post by id
 destroy(1);
